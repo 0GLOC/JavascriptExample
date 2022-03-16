4853c11a84e5e9e46f83023ee6a9a4360e985077
momia15 <GabiMaidanaxD@hotmail.com>
2022-03-15 21:29:13 -0300

    Arreglo LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-//Productos
-const games = [
-    { producto: 'game', name: 'game 1', price: 500},
-    { producto: 'game', name: 'game 2', price: 250},
-    { producto: 'game', name: 'game 3', price: 1200},
-    { producto: 'game', name: 'game 4', price: 400},
-]
-
-localStorage.setItem("Games" ,JSON.stringify(games));
-let storageGames = JSON.parse(localStorage.getItem("Games"));
-console.log(storageGames);
-
 //JSON
 function allProductsProgramar(){
     allProducts();
@@ -24,18 +12,20 @@ function allProducts(){
         if(this.readyState == 4 && this.status == 200){
             let datosJson = JSON.parse(this.responseText);
             let res = document.getElementById("tabla");
+            //localstorage json ("agregar todos los productos")
+            localStorage.setItem("carrito" ,JSON.stringify(datosJson));
+            let productoCarrito = JSON.parse(localStorage.getItem("carrito"));
+            console.log(productoCarrito);
 
             for(let item of datosJson){
                 //item. (lo que desee visualizar de mi JSON)
                 //console.log(item)
                 res.innerHTML += `<tr>
                 <td>${item.name}</td>
-                <td>${item.price}</td>
-                <td>$</td>
+                <td>$${item.price}</td>
                 </tr>`;
             };
         };
-
     };
 };
 
@@ -51,11 +41,6 @@ function capturar(){
     //variables
     let nombreCapturar = document.getElementById("nombre").value;
     let precioCapturar = 0;
-
-    //Localstorage
-    localStorage.setItem("Array" ,JSON.stringify(nombreCapturar));
-    let arrayRecojido = JSON.parse(localStorage.getItem("Array"));
-    console.log(arrayRecojido);
 
     //Datos primitivos
     if (nombreCapturar === "game 1"){
@@ -81,6 +66,11 @@ function capturar(){
 
     nuevoSujeto = new persona(nombreCapturar,precioCapturar);
     console.log(nuevoSujeto);
+
+    //LocalStorage ("seleccionar producto")
+    localStorage.setItem("escritura" ,JSON.stringify(nuevoSujeto));
+    let productoEscritura = JSON.parse(localStorage.getItem("carrito"));
+    console.log(productoEscritura);
     
     //Función a invocar
     agregar();
@@ -89,12 +79,11 @@ function capturar(){
 //Array
 let baseDatos= [];
 
-
 //invocación de funcion
 function agregar(){
     baseDatos.push(nuevoSujeto);
     console.log(baseDatos);
-    document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevoSujeto.nombre+'</td><td>$'+nuevoSujeto.precio+'</td><td>$'+" "+'</td></tbody>';
+    document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevoSujeto.nombre+'</td><td>$'+nuevoSujeto.precio+'</td></tbody>';
 };
 
 //DOM (Creación de H2 (titulo) mediante createElement)
@@ -129,17 +118,17 @@ function Boton(){
 };
 
 function firstBoton(){
-    document.getElementById("tabla").innerHTML += '<tbody><td>'+'game1'+'</td><td>$'+'500'+'</td><td>$'+" "+'</td></tbody>';
+    document.getElementById("tabla").innerHTML += '<tbody><td>'+'game1'+'</td><td>$'+'500'+'</td></tbody>';
 };
 
 function secondBoton(){
-    document.getElementById("tabla").innerHTML += '<tbody><td>'+'game2'+'</td><td>$'+'250'+'</td><td>$'+" "+'</td></tbody>';
+    document.getElementById("tabla").innerHTML += '<tbody><td>'+'game2'+'</td><td>$'+'250'+'</td></tbody>';
 };
 
 function thirdBoton(){
-    document.getElementById("tabla").innerHTML += '<tbody><td>'+'game3'+'</td><td>$'+'1200'+'</td><td>$'+" "+'</td></tbody>';
+    document.getElementById("tabla").innerHTML += '<tbody><td>'+'game3'+'</td><td>$'+'1200'+'</td></tbody>';
 };
 
 function fourthBoton(){
-    document.getElementById("tabla").innerHTML += '<tbody><td>'+'game4'+'</td><td>$'+'400'+'</td><td>$'+" "+'</td></tbody>';
+    document.getElementById("tabla").innerHTML += '<tbody><td>'+'game4'+'</td><td>$'+'400'+'</td></tbody>';
 };
